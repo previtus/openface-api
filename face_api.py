c0eba47bfd9e9c7f49edbf1dcd06eb6efdceea7f
previtus <previtus@gmail.com>
2018-07-19 18:11:16 -0400

    notes from my initial tests of making it run properly

--- face_api.py
+++ face_api.py
@@ -14,12 +14,15 @@ def hello_world():
 
 @app.route('/compare/', methods=['GET','POST'])
 def login():    
-    if not 'img1' in request.form:
-         return "Img1 param missing", 400
-    if not 'img2' in request.form:
-         return "Img2 param missing", 400
-    img1 = request.form['img1']
-    img2 = request.form['img2']
+    img1 = request.args.get("img1")
+    img2 = request.args.get("img2")
+
+    #if not 'img1' in request.form:
+    #     return "Img1 param missing", 400
+    #if not 'img2' in request.form:
+    #     return "Img2 param missing", 400
+    #img1 = request.form['img1']
+    #img2 = request.form['img2']
     distance = face_compare.perform(img1, img2)
     dat = json.dumps({"distance": distance})
     resp = Response(response = dat,
